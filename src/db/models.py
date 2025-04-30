686be92e4835c19d389ec1499189601f259a8e70
AlexeyShchukin <shchukin.developer@gmail.com>
2025-04-30 18:45:18 +0200

    date calculation logic moved to api

--- src/db/models.py
+++ src/db/models.py
@@ -1,10 +1,11 @@
 from uuid import UUID, uuid4
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
-from sqlalchemy import DateTime, text, String, ForeignKey, Boolean
+from sqlalchemy import DateTime, String, ForeignKey, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from src.core.config import settings
 from src.db.database import Base
 
 
@@ -21,7 +22,8 @@ class Task(Base):
     completed: Mapped[bool] = mapped_column(Boolean, default=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
-        server_default=text("TIMEZONE('utc', now())")
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False
     )
     owner_uuid: Mapped[UUID] = mapped_column(ForeignKey("users.uuid"), nullable=False)
 
@@ -55,9 +57,13 @@ class RefreshToken(Base):
     token: Mapped[str] = mapped_column(String(500), nullable=False, unique=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
-        server_default=text("TIMEZONE('utc', now())")
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False
     )
-    expires_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS),
+        nullable=False)
     ip_address: Mapped[str] = mapped_column(String(50), nullable=True)
     user_agent: Mapped[str] = mapped_column(String(200))
     is_active: Mapped[bool] = mapped_column(default=True)
