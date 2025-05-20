5fff2f32cc6883de240ecdc071a58f9f312978de
AlexeyShchukin <shchukin.developer@gmail.com>
2025-05-20 20:50:27 +0200

    updated handlers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi.exceptions import RequestValidationError
-from sqlalchemy.exc import DatabaseError
+from sqlalchemy.exc import SQLAlchemyError
 from uvicorn import run
 from fastapi import FastAPI
 
@@ -9,7 +9,7 @@ from src.api.endpoints.tasks import task_router
 from src.api.endpoints.users import user_router
 from src.api.middleware.middleware import LoggingMiddleware, configure_cors
 from src.core.lifespan import lifespan
-from src.exceptions.handlers import validation_exception_handler, handle_db_error
+from src.exceptions.handlers import validation_exception_handler, handle_db_error, handle_unexpected_error
 
 app = FastAPI(lifespan=lifespan)
 
@@ -19,7 +19,8 @@ app.include_router(task_router)
 app.include_router(ws_router)
 
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
-app.add_exception_handler(DatabaseError, handle_db_error)
+app.add_exception_handler(SQLAlchemyError, handle_db_error)
+app.add_exception_handler(Exception, handle_unexpected_error)
 app.add_middleware(LoggingMiddleware)
 configure_cors(app)
 
--- src/loggers/loggers.py
+++ src/loggers/loggers.py
@@ -5,19 +5,14 @@ def setup_logger():
     log = logging.getLogger(__name__)
     log.setLevel(logging.INFO)
 
-    consol_handler = logging.StreamHandler()
-    consol_handler.setLevel(logging.INFO)
-
     file_handler = logging.FileHandler("app.log", "w", encoding="utf-8")
     file_handler.setLevel(logging.INFO)
 
     formatter = logging.Formatter(datefmt="%Y-%m-%d %H:%M:%S",
         fmt="[%(asctime)s.%(msecs)03d] %(filename)18s:%(lineno)-3d %(levelname)-8s - %(message)s"
     )
-    consol_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
 
-    log.addHandler(consol_handler)
+    file_handler.setFormatter(formatter)
     log.addHandler(file_handler)
 
     return log
