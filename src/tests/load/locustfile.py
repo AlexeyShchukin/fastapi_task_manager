04fad9f65ce85772dafa3a7d8180c7cefec0ba9b
AlexeyShchukin <shchukin.developer@gmail.com>
2025-06-23 23:19:34 +0200

    updated readme and locust

--- src/tests/load/locustfile.py
+++ src/tests/load/locustfile.py
@@ -1,28 +1,33 @@
 from locust import HttpUser, task, between
 
+from threading import Lock
 
+token_lock = Lock()
+shared_token = None
 
 class SingleUserLoad(HttpUser):
     wait_time = between(1, 3)
 
     def on_start(self):
-        payload = {
-            "username": "Alex",
-            "password": "My_pass1"
-        }
-        response = self.client.post(
-            "/api/v1/auth/login/",
-            data=payload,
-            headers={"Content-Type": "application/x-www-form-urlencoded"}
-        )
-        print("Login response:", response.status_code, response.text)
+        global shared_token
+        with token_lock:
+            if shared_token is None:
+                payload = {
+                    "username": "Alex",
+                    "password": "My_pass1"
+                }
+                response = self.client.post(
+                    "/api/v1/auth/login/",
+                    data=payload,
+                    headers={"Content-Type": "application/x-www-form-urlencoded"}
+                )
+                if response.status_code != 200:
+                    raise Exception("Login failed")
+                tokens = response.json()
+                shared_token = tokens["access_token"]
 
-        if response.status_code != 200:
-            raise Exception("Login failed")
+        self.headers = {"Authorization": f"Bearer {shared_token}"}
 
-        tokens = response.json()
-        self.access_token = tokens["access_token"]
-        self.headers = {"Authorization": f"Bearer {self.access_token}"}
 
     @task
     def get_tasks(self):
